18db3f9bb613560bdc36317c922b0d705ce04717
Jared Truscott <Jtruscott78@gmail.com>
2017-12-16 18:34:28 -0500

    created dropdown logic

--- scripts/main.js
+++ scripts/main.js
@@ -2,70 +2,86 @@
 // var RapidAPI = new require('rapidapi-connect');
 var rapid = new RapidAPI('storeranking', '9ff7edb4-d7e5-43f8-92d2-4fb7f22a46eb');
 
+//holds district numbers and uses them to build drop-down list and call JSON file from storeinfo.js
+var districts = ['2806', '2807'];
+
 $(document).ready(function () {
 
-  //loads data for the store information from storeinfo.js ... this is used to keep things 'clean' as eventually this app will contain over 100 stores and an option to select each district .. eventually this will work best as an API request to a server to get all store information.
+  //build drop-down based on storeInfo
+  (function () {
+    for (i = 0; i < districts.length; i++) {
 
-  //All API calls should be served directly from the server, I have them in JS for demo purposes
-
-  //Yelp API call using rapid api
-  function getInfoYelp(storeInfo, ratingSource) {
-
-    rapid.call('YelpAPI', 'getSingleBusiness', {
-      'accessToken': '0KFFwb0CeOocTb-DTHcHAIyHNVoh4x4I_TZ5mkoUM4RYPyzG2ZfSgCZcBsthpgJBJGTBGeQFPVCbzA5sIt8zG3y3_cU3upPRT13G3okGWRuzG27p8K9jvnqYbjMkWnYx',
-      'bussinessId': storeInfo[ratingSource]
-
-    }).on('success', function (data) {
-      rating = data.rating
-      $('#' + storeInfo.sNumber + ratingSource).text(rating.toFixed(1) + ' / 5');
-    });
-  };
-
-  //Google API call
-  function getInfoGoogle(storeInfo, ratingSource) {
-    // Proxy to bypass CORS
-    proxy = 'https://cors-anywhere.herokuapp.com/';
-    googleApi = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
-    googleKey = '&key=AIzaSyALgMeJoWoeLiygtjWOu1uRou7vJRzQg0I';
-
-    $.get(proxy + googleApi + storeInfo[ratingSource] + googleKey,
-      function (data) {
-        rating = data.result.rating;
-        $('#' + storeInfo.sNumber + ratingSource).text(rating.toFixed(1) + ' / 5');
-      }
-    );
-  }
-
-  //Facebook API call
-  function getInfoFacebook(storeInfo, ratingSource) {
-    $.ajax({
-      url: 'https://graph.facebook.com/v2.11/' + storeInfo[ratingSource] + '?fields=overall_star_rating',
-      beforeSend: function (xhr) {
-        xhr.setRequestHeader("Authorization", "Bearer EAAb6s2uIpEMBAAbSvg1vOWop15RpXsmBH9meE59kCuRh0JxzA1eZCxxWbnCEHqpLZCSNKmPSj7OuDFx1RhBC8pO1y8ZA9GBCcEAEkDZC3S0bZCkflZCFGr5GJ8ol8SMZAQ4Bkb5vYAZCC4LcvBiff8UgZC4ohbkfj0dwZD")
-      },
-      success: function (data) {
-        rating=(data.overall_star_rating);
-        $('#' + storeInfo.sNumber + ratingSource).text(rating.toFixed(1) + ' / 5');
-      }
-    })
-  }
-
-  /* creates table rows based on length of array and fills store information */
-  function makeTable() {
-    for (i = 0; i < storeInfo.length; i++) {
-      $('#ranking_table').append(
-        '<tr>' +
-        '<td class="store-number">' + storeInfo[i].sNumber + '</td>' +
-        '<td id=' + storeInfo[i].sNumber + 'yelpId> <p>-</p> </td>' +
-        '<td id=' + storeInfo[i].sNumber + 'googleId><p>-</p> </td>' +
-        '<td id=' + storeInfo[i].sNumber + 'facebookId> <p>-</p> </td>' +
-        '</tr>'
-      );
-
-      getInfoYelp(storeInfo[i], "yelpId");
-      getInfoGoogle(storeInfo[i], "googleId");
-      getInfoFacebook(storeInfo[i], "facebookId");
+      $('#district-selection').append(
+        '<option value=d' + districts[i] + '>' + districts[i] + '</option>')
     }
-  }
-  makeTable();
+  })();
+
 });
+
+
+//All API calls should be served directly from the server, I have them in JS for demo purposes
+
+//Yelp API call using rapid api
+function getInfoYelp(storeInfo, ratingSource) {
+
+  rapid.call('YelpAPI', 'getSingleBusiness', {
+    'accessToken': '0KFFwb0CeOocTb-DTHcHAIyHNVoh4x4I_TZ5mkoUM4RYPyzG2ZfSgCZcBsthpgJBJGTBGeQFPVCbzA5sIt8zG3y3_cU3upPRT13G3okGWRuzG27p8K9jvnqYbjMkWnYx',
+    'bussinessId': storeInfo[ratingSource]
+
+  }).on('success', function (data) {
+    rating = data.rating
+    $('#' + storeInfo.sNumber + ratingSource).text(rating.toFixed(1) + ' / 5');
+  });
+};
+
+//Google API call
+function getInfoGoogle(storeInfo, ratingSource) {
+  // Proxy to bypass CORS
+  proxy = 'https://cors-anywhere.herokuapp.com/';
+  googleApi = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
+  googleKey = '&key=AIzaSyALgMeJoWoeLiygtjWOu1uRou7vJRzQg0I';
+
+  $.get(proxy + googleApi + storeInfo[ratingSource] + googleKey,
+    function (data) {
+      rating = data.result.rating;
+      $('#' + storeInfo.sNumber + ratingSource).text(rating.toFixed(1) + ' / 5');
+    }
+  );
+}
+
+//Facebook API call
+function getInfoFacebook(storeInfo, ratingSource) {
+  $.ajax({
+    url: 'https://graph.facebook.com/v2.11/' + storeInfo[ratingSource] + '?fields=overall_star_rating',
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer EAAb6s2uIpEMBAAbSvg1vOWop15RpXsmBH9meE59kCuRh0JxzA1eZCxxWbnCEHqpLZCSNKmPSj7OuDFx1RhBC8pO1y8ZA9GBCcEAEkDZC3S0bZCkflZCFGr5GJ8ol8SMZAQ4Bkb5vYAZCC4LcvBiff8UgZC4ohbkfj0dwZD")
+    },
+    success: function (data) {
+      rating = (data.overall_star_rating);
+      $('#' + storeInfo.sNumber + ratingSource).text(rating.toFixed(1) + ' / 5');
+    }
+  })
+}
+
+/* creates table rows based on length of array and fills store information */
+
+// will load information based on 'value' assigned above
+
+function makeTable() {
+  for (i = 0; i < d2806.length; i++) {
+
+    $('#ranking_table').append(
+      '<tr>' +
+      '<td class="store-number">' + d2806[i].sNumber + '</td>' +
+      '<td id=' + d2806[i].sNumber + 'yelpId> <p>-</p> </td>' +
+      '<td id=' + d2806[i].sNumber + 'googleId><p>-</p> </td>' +
+      '<td id=' + d2806[i].sNumber + 'facebookId> <p>-</p> </td>' +
+      '</tr>'
+    );
+
+    //getInfoYelp(storeInfo[0][i], "yelpId");
+    //getInfoGoogle(storeInfo[0][i], "googleId");
+    //getInfoFacebook(storeInfo[0][i], "facebookId");
+  }
+}
+makeTable();
--- scripts/storeinfo.js
+++ scripts/storeinfo.js
@@ -1,5 +1,4 @@
-var storeInfo = [
-  {
+var d2806 = [{
     sNumber: 1226,
     googleId: "ChIJIRWkw4xzy4kRWYGJyLNtaX0",
     yelpId: "advance-auto-parts-ebensburg",
@@ -95,5 +94,102 @@ var storeInfo = [
     yelpId: "advance-auto-parts-tyrone",
     facebookId: "AdvanceAutoPartsStore7817"
   }
+];
 
+var d2807 = [{
+    sNumber: 12,
+    googleId: "ChIJIRWkw4xzy4kRWYGJyLNtaX0",
+    yelpId: "advance-auto-parts-ebensburg",
+    facebookId: "AdvanceAutoPartsStore1226"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJGzKO-Rrjy4kR8EQPqZzaRkE",
+    yelpId: "advance-auto-parts-huntingdon-3",
+    facebookId: "AdvanceAutoPartsStore1227"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJBwhMltsQy4kRDVSMSKjIUAc",
+    yelpId: "advance-auto-parts-johnstown",
+    facebookId: "AdvanceAutoPartsStore1243"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJm0HhIsFKyYkRJeF9njh6R_I",
+    yelpId: "advance-auto-parts-lewistown",
+    facebookId: "AdvanceAutoPartsStore1247"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJaw96zhpNyokRdlNTPFp29eI",
+    yelpId: "advance-auto-parts-everett",
+    facebookId: "AdvanceAutoPartsStore1251"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJTQGB3cETzIkRSeAvtUOgW2g",
+    yelpId: "advance-auto-parts-philipsburg",
+    facebookId: "AdvanceAutoPartsStore1257"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJrVtdI0CRy4kRa22dPA7BJVE",
+    yelpId: "advance-auto-parts-altoona",
+    facebookId: "AdvanceAutoPartsStore1261"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJ56kLWkAQy4kRTbD23YkwnDE",
+    yelpId: "advance-auto-parts-johnstown-2",
+    facebookId: "AdvanceAutoPartsStore1262"
+  },
+  {
+    sNumber: 12,
+    googleId: "ChIJI-kiajrcyokRCbLnyB6jLmU",
+    yelpId: "advance-auto-parts-somerset",
+    facebookId: "AdvanceAutoPartsStore1272"
+  },
+  {
+    sNumber: 53,
+    googleId: "ChIJISO5DNO9y4kRi3J6d05-TEo",
+    yelpId: "advance-auto-parts-roaring-spring",
+    facebookId: "AdvanceAutoPartsStore5345"
+  },
+  {
+    sNumber: 55,
+    googleId: "ChIJdxHBJi3yzokRqRb2e4GekNc",
+    yelpId: "advance-auto-parts-mill-hall",
+    facebookId: "AdvanceAutoPartsStore5531"
+  },
+  {
+    sNumber: 60,
+    googleId: "ChIJhS-kLtenzokRosA9Bx7w4UY",
+    yelpId: "advance-auto-parts-state-college",
+    facebookId: "AdvanceAutoPartsStore6016"
+  },
+  {
+    sNumber: 63,
+    googleId: "ChIJA6ZVrCGSy4kRM9xA-hU4i04",
+    yelpId: "advance-auto-parts-altoona-2",
+    facebookId: "AdvanceAutoPartsStore6304"
+  },
+  {
+    sNumber: 66,
+    googleId: "ChIJTc0tbqkWy4kR8vLNgT3ZhxY",
+    yelpId: "advance-auto-parts-johnstown-4",
+    facebookId: "AdvanceAutoPartsStore6629"
+  },
+  {
+    sNumber: 73,
+    googleId: "ChIJixu4ybKlzokRdspXJkWbjps",
+    yelpId: "advance-auto-parts-state-college-2",
+    facebookId: "AdvanceAutoPartsStore7302"
+  },
+  {
+    sNumber: 78,
+    googleId: "ChIJyaoY7IX1y4kRYSaF6tXuucw",
+    yelpId: "advance-auto-parts-tyrone",
+    facebookId: "AdvanceAutoPartsStore7817"
+  }
 ];
