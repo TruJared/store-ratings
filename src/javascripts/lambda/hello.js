c2fb86539cc37978989007d6ff5fc2ca60f479a2
Jared Truscott <Jtruscott78@gmail.com>
2018-04-30 15:57:44 -0400

    removed netlify functions

--- src/javascripts/lambda/hello.js
+++
@@ -1,7 +0,0 @@
-exports.handler = function (event, context, callback) {
-  const superDuperSecretApi = process.env.SECRET;
-  callback(null, {
-    statusCode: 200,
-    body: `Hello ${superDuperSecretApi}`,
-  });
-};
