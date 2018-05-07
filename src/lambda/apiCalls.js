57c86bf8033511456e282ad2707702f04c700e3c
Jared Truscott <Jtruscott78@gmail.com>
2018-05-07 12:22:18 -0400

    logging key (yikes)

--- src/lambda/apiCalls.js
+++ src/lambda/apiCalls.js
@@ -34,7 +34,7 @@ exports.handler = (event, context, callback) => {
       callback(null, {
         statusCode,
         headers,
-        body: res,
+        body: googleKey,
       }))
     .catch(e =>
       callback(null, {
